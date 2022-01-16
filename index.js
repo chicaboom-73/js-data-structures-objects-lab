a63761a1581a89729d17d4f011260221703bc4fa
claudia welch <claudia.welch73@gmail.com>
2022-01-17 10:54:46 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const driver = {name: "Sam"};
+
+function updateDriverWithKeyAndValue(obj, key, value){
+    return Object.assign({},obj,{[key]:value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+    return Object.assign(obj,{[key]:value});
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newObject = {...driver};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver;
+}
